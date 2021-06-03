8870a1ccf8a7ac8276f93b978e8255f42b4484a1
Darren White <dwhite@gentingcasinos.co.uk>
2021-06-03 10:10:52 +0100

    Add clubhouse token

--- index.js
+++ index.js
@@ -10,6 +10,23 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
+
+  const data = {
+    "author_id": "5dc6e1f7-cdad-4fda-8239-aed56d7dbc56",
+    "created_at": "2021-06-03T09:00:00.001Z",
+    text: 'PR from custom GitHub action'
+  }
+
+  const addComment = await fetch('https://api.clubhouse.io/api/v3/stories/2096/comments', {
+    method: 'post',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json',
+      'Clubhouse-Token': core.getInput('clubhouse-token'),
+    }
+  });
+
+  console.log(addComment)
 } catch (error) {
   core.setFailed(error.message);
 }
