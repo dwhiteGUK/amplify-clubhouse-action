3dc0f7d3410c24a461b737edad986d3163924fc0
Darren White <dwhite@gentingcasinos.co.uk>
2021-06-03 11:36:47 +0100

    Mock clubhouse comment code

--- index.js
+++ index.js
@@ -1,32 +1,30 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+import core from '@actions/core';
+import github from '@actions/github';
+import fetch from 'node-fetch';
+import dotenv from 'dotenv'
+dotenv.config()
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-
-  const data = {
-    "author_id": "5dc6e1f7-cdad-4fda-8239-aed56d7dbc56",
-    "created_at": "2021-06-03T09:00:00.001Z",
-    text: 'PR from custom GitHub action'
-  }
-
-  const addComment = await fetch('https://api.clubhouse.io/api/v3/stories/2096/comments', {
-    method: 'post',
-    body: JSON.stringify(data),
-    headers: {
-      'Content-Type': 'application/json',
-      'Clubhouse-Token': core.getInput('clubhouse-token'),
+async function main() {
+  try {
+    const data = {
+      "author_id": "5dc6e1f7-cdad-4fda-8239-aed56d7dbc56",
+      "created_at": "2021-06-03T09:00:00.001Z",
+      text: `PR from custom GitHub action ${Date.now()}`
     }
-  });
 
-  console.log(addComment)
-} catch (error) {
-  core.setFailed(error.message);
+    const addComment = await fetch('https://api.clubhouse.io/api/v3/stories/2096/comments', {
+      method: 'post',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+        'Clubhouse-Token': process.env.NODE_ENV === 'dev' ? process.env.CLUBHOUSE_ACTIONS_API_TOKEN : core.getInput('clubhouse-token'),
+      }
+    });
+
+    console.log(addComment)
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+main()
