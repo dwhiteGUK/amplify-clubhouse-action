68efd8a696f6d718cb86999a994523352e5f19d7
Darren White <dwhite@gentingcasinos.co.uk>
2021-06-08 09:02:07 +0100

    Test job input

--- index.js
+++ index.js
@@ -29,29 +29,28 @@ async function main() {
       event_name
     } = core.getInput('github-context')
 
-    if (event_name === 'pull_request') {
 
-      const storyId = matchStoryId(head_ref)
+    const storyId = matchStoryId(head_ref)
 
-      const data = {
-        "created_at": event.pull_request.created_at,
-        text: `Preview available on https://pr-${event.number}.${process.env.NODE_ENV === 'dev' ? process.env.AMPLIFY_PROJECT_ID : core.getInput('amplify-project-id')}.amplifyapp.com`
-      }
-      console.log('🚀 ~ file: index.js ~ line 39 ~ main ~ data', data)
-
-      console.log(`The data: ${payload}`);
-
-      const addComment = await fetch(`https://api.clubhouse.io/api/v3/stories/${storyId}/comments`, {
-        method: 'post',
-        body: JSON.stringify(data),
-        headers: {
-          'Content-Type': 'application/json',
-          'Clubhouse-Token': process.env.NODE_ENV === 'dev' ? process.env.CLUBHOUSE_ACTIONS_API_TOKEN : core.getInput('clubhouse-token'),
-        }
-      });
-
-      console.log(`Reponse: ${addComment}`);
+    const data = {
+      "created_at": event.pull_request.created_at,
+      text: `Preview available on https://pr-${event.number}.${process.env.NODE_ENV === 'dev' ? process.env.AMPLIFY_PROJECT_ID : core.getInput('amplify-project-id')}.amplifyapp.com`
     }
+    console.log('🚀 ~ file: index.js ~ line 39 ~ main ~ data', data)
+
+    console.log(`The data: ${payload}`);
+
+    const addComment = await fetch(`https://api.clubhouse.io/api/v3/stories/${storyId}/comments`, {
+      method: 'post',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+        'Clubhouse-Token': process.env.NODE_ENV === 'dev' ? process.env.CLUBHOUSE_ACTIONS_API_TOKEN : core.getInput('clubhouse-token'),
+      }
+    });
+
+    console.log(`Response: ${addComment}`);
+
 
     return;
   } catch (error) {
